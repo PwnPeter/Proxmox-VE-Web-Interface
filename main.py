d13af0682e949fdba92dc6318ec2c3ef2c1161d4
Valentin <valentin.bonneau.pro+dev@gmail.com>
2020-07-10 11:30:46 +0200

    upload ok

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 # Max 2 Mo les fichiers
+
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
 def index():
@@ -17,8 +26,29 @@ def dashboard():
 @app.route('/upload', methods=['POST'])
 def upload_csv():
     """récupère le csv et les choix de génération des VM (classe + os)"""
+    
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        print(1)
+        return 404
+        
+    file = request.files['file']
 
-    return 'cc'
+    # if user does not select file, browser also
+    # submit an empty part without filename
+
+    print(file.filename)
+    if file.filename == '':
+        print(2)
+        return 404
+
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)   # remove directory traversing, encoded caracters, space etc
+
+        print(file.read().decode())
+
+    return 200
 
 @app.route('/delete', methods=['PUT'])
 def delete_class():
